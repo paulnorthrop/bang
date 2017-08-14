922997cafeba1048dc35706f9226a04adf8a5189
Paul Northrop <p.northrop@ucl.ac.uk>
2017-08-14 08:57:01 +0100

    Update to include 1-way ANOVA.

--- R/set_and_check_prior.R
+++ R/set_and_check_prior.R
@@ -1,3 +1,7 @@
+# For an ANOVA model set the dimension of the prior:
+#  1-way: 2 for a prior for (sigma_alpha, sigma) only
+#         3 for a prior for (mu, sigma_alpha, sigma)
+
 # ========================== set_user_prior ===================================
 
 #' Set a user-defined prior
@@ -10,11 +14,13 @@
 #' @param ... Further arguments giving the names and values of any
 #'   parameters involved in the function \code{prior}.
 #' @export
-set_user_prior <- function(prior, model = c("binom_beta", "pois_gamma"), ...) {
+set_user_prior <- function(prior, model = c("binom_beta", "pois_gamma",
+                                            "anova"), anova_d = 2, ...) {
   if (!is.function(prior)) {
     stop("prior must be a function")
   }
   model <- match.arg(model)
+  prior$anova_d <- anova_d
   # Return a list and add additional arguments from ....
   temp <- list(prior = prior, ...)
   return(structure(temp, class = "bang_prior", model = model))
@@ -22,10 +28,29 @@ set_user_prior <- function(prior, model = c("binom_beta", "pois_gamma"), ...) {
 
 # ============================ check_prior ====================================
 
-check_prior <- function(prior, model, hpars) {
+check_prior <- function(prior, model, hpars, n_groups = NULL) {
+  # If prior is a character scalar then a default prior is ebing requested
   if (is.character(prior)) {
     prior_name <- prior
     prior <- list()
+    # One-way ANOVA
+    if (model == "one_way_anova") {
+      if (prior_name %in% c("default", "bda") & n_groups < 3) {
+        stop("Need >= 3 groups for posterior propriety if bda prior used")
+      }
+      prior$prior <- switch(prior_name,
+                            default = anova1_bda_prior,
+                            bda = anova1_bda_prior,
+                            unif = anova1_unif_prior,
+                            cauchy = anova1_norm_cauchy_prior)
+      prior$anova_d <- 2
+      if (prior_name == "cauchy") {
+        prior$hpars <- hpars
+        if (is.null(hpars)) {
+          prior$hpars <- anova1_cauchy_hpars()
+        }
+      }
+    }
     # Binomial-beta
     if (model == "binom_beta") {
       prior$prior <- switch(prior_name,
@@ -39,6 +64,7 @@ check_prior <- function(prior, model, hpars) {
         }
       }
     }
+    # Poisson-gamma
     if (model == "pois_gamma") {
       prior$prior <- switch(prior_name,
                             default = gamma_gamma_prior,
