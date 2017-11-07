6aa102af903de74106e9226a15ed1400e3ec26b1
Paul Northrop <p.northrop@ucl.ac.uk>
2017-11-07 22:43:10 +0000

    Correct indices. [ci skip]

--- R/anova_prior.R
+++ R/anova_prior.R
@@ -17,9 +17,8 @@ anova1_unif_prior <- function(x) {
 
 anova1_cauchy_prior <- function(x, hpars) {
   if (x[1] <= 0 | x[2] <= 0) return(-Inf)
-  print(hpars)
-  log_sigma_alpha <- stats::dcauchy(x[2], scale = hpars[3], log = TRUE)
-  log_sigma <- stats::dcauchy(x[3], scale = hpars[4], log = TRUE)
+  log_sigma_alpha <- stats::dcauchy(x[1], scale = hpars[1], log = TRUE)
+  log_sigma <- stats::dcauchy(x[2], scale = hpars[2], log = TRUE)
   return(log_sigma_alpha + log_sigma)
 }
 
