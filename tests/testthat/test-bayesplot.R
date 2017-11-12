2fda02aae2b2a5658acbc858c0966848ea56a834
Paul Northrop <p.northrop@ucl.ac.uk>
2017-11-12 17:45:06 +0000

    Add bayesplot test.

---
+++ tests/testthat/test-bayesplot.R
@@ -0,0 +1,24 @@
+context("bayesplot")
+
+my_seed <- 47
+my_tol <- 1e-5
+my_n <- 5
+my_prob <- 0.5
+
+# ------------------------- Beta-binomial model, rat data ------------------- #
+
+rat_res <- hef(model = "beta_binom", data = rat, n = 10)
+
+# Check that not supplying nrep to hef gives error
+check_error <- try(pp_check(rat_res), silent = TRUE)
+test_that("pp_check: error when nrep missing", {
+  testthat::expect_identical(class(check_error), "try-error")
+})
+
+rat_res <- hef(model = "beta_binom", data = rat, n = 10, nrep = 10)
+
+# Check that pp_check returns a ggplot object
+check_ggplot <- "ggplot" %in% class(pp_check(rat_res))
+test_that("pp_check: check that ggplot object is returned", {
+  testthat::expect_identical(check_ggplot, TRUE)
+})
