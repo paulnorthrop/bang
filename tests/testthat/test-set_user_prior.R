6373d75c42bb51139d60e5049d905c4182e715bc
Paul Northrop <p.northrop@ucl.ac.uk>
2017-11-19 21:59:44 +0000

    Add test.

---
+++ tests/testthat/test-set_user_prior.R
@@ -0,0 +1,8 @@
+context("set_user_prior")
+
+# Non-function prior
+check_error <- try(set_user_prior("bda"), silent = TRUE)
+test_that("set_user_prior: character prior gives error", {
+  testthat::expect_identical(class(check_error), "try-error")
+})
+
